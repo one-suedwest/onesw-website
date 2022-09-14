719649dd0fffabac9ec93c92dbc18c9f6657ba73
Fabian Letsch <28201880+Anoian@users.noreply.github.com>
2022-09-14 22:14:57 +0200

    add astro-robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
-
+import robotsTxt from 'astro-robots-txt';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), sitemap()]
+  site: 'https://clever-pothos-e6b088.netlify.app/',
+  experimental: {
+    integrations: true,
+  },
+  integrations: [compress(), sitemap(), robotsTxt({
+    policy: [
+      { disallow: '/', userAgent: '*' },
+    ]
+  })]
 });
