99fa1ea82aca464d680704a862b78cc652b2e684
Fabian Letsch <28201880+Anoian@users.noreply.github.com>
2022-09-14 21:54:23 +0200

    add @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import compress from "astro-compress";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
+  integrations: [compress(), sitemap()]
 });
