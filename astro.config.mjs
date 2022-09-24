d79e4547b2ac7d881b4f9f4284b08941918cadb4
Fabian Letsch <28201880+Anoian@users.noreply.github.com>
2022-09-24 22:55:07 +0200

    make robots.txt allow everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   integrations: [compress(), sitemap(), robotsTxt({
     policy: [{
-      disallow: '/',
+      allow: '/',
       userAgent: '*'
     }]
   })]
