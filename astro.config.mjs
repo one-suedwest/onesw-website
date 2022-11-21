69896b892e5ca88860f3f01f610dd352f936854c
Fabian Letsch <28201880+Anoian@users.noreply.github.com>
2022-11-21 19:53:46 +0100

    fix pdf handling with google

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,10 @@ export default defineConfig({
       {
         disallow: '/datenschutz',
         userAgent: '*'
+      },
+      {
+        disallow: '/pdf/',
+        userAgent: '*'
       }
     ]
   })]
