6f34dbaca7507aabd5191397a9166a7608b4170a
Fabian Letsch <28201880+Anoian@users.noreply.github.com>
2023-08-15 18:15:32 +0200

    Update all packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.onesw.de/',
-  experimental: {
-    integrations: true
-  },
   integrations: [compress(), sitemap(), robotsTxt({
     policy: [
       {
