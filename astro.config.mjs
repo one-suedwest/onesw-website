a362250d0639a41a2c89654d6e8651e4bb610ce5
Fabian Letsch <28201880+Anoian@users.noreply.github.com>
2024-05-16 23:03:49 +0200

    add github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.onesw.de/',
+  site: 'https://onesw.de/',
   integrations: [compress(), sitemap(), robotsTxt({
     policy: [
       {
